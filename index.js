ba85c2164fd6621a16b689cb71e0640bedb7fa9a
otal18 <hakan.otal@outlook.com>
2020-08-30 22:40:08 +0300

    v1.1

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-const API = "https://numerous-server.herokuapp.com/api/v1/login";
+const API = "https://numerous-server.herokuapp.com/api/v1/";
 const fetch = require("node-fetch");
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const port = process.env.PORT || 3000;
 
 const app = express();
 const router = express.Router();
 
-//TODO USER SPECIFIED
-app.set('isLogin', false);
-let userId, userNum, userMail, userPassword;
+app.use(express.static(__dirname + '/public'));
+app.engine('html', require('ejs').renderFile);
 
-app.use(express.static(__dirname + "/public"));
+let loggedUsers = [];
 
-//LOGIN - SIGN UP
+//INDEX
 router.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/pages/index.html"));
 });
 
 //HOME
-router.get("/home", function (req, res) {
-  if (!app.get('isLogin')) res.redirect("/");
-  else res.sendFile(path.join(__dirname + "/pages/home.html"));
+router.get("/home/:userId/:userNum", function (req, res) {
+  if (loggedUsers.includes(req.params.userId)) {
+    res.render(path.join(__dirname + "/pages/home.html"), {
+      id: req.params.userId,
+      number: req.params.userNum
+    })
+  } else res.redirect("/");
 });
 
 //PROFILE
-router.get("/profile", function (req, res) {
-  if (!isLogin) res.redirect("/");
-  else res.sendFile(path.join(__dirname + "/pages/profile.html"));
+router.get("/profile/:userId/:userNum", function (req, res) {
+  if (loggedUsers.includes(req.params.userId)) {
+    res.render(path.join(__dirname + "/pages/profile.html"), {
+      id: req.params.userId,
+      number: req.params.userNum
+    })
+  } else res.redirect("/");
 });
 
 //MESSAGES
 router.get("/messages", function (req, res) {
+  //TODO
   res.sendStatus(404);
 });
 
 //LOGOUT
-router.get("/logout", function (req, res) {
-  isLogin = false;
-  userId = null;
-  userNum = null;
-  userMail = null;
-  userPassword = null;
+router.get("/logout/:userId", function (req, res) {
+  console.log("user logout: " + req.params.userId);
+  for (let i = 0; i < loggedUsers.length; i++)
+    if (loggedUsers[i] === req.params.userId) loggedUsers.splice(i, 1);
   res.redirect("/");
 });
 
@@ -52,7 +59,7 @@ router.use(
   })
 );
 router.post("/login", function (req, res) {
-  fetch(API, {
+  fetch(API + "login", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -60,22 +67,66 @@ router.post("/login", function (req, res) {
       },
       body: JSON.stringify({
         mail: req.body.mail,
-        password: req.body.password,
+        password: req.body.password
       }),
     })
     .then((response) => {
       if (response.ok) {
         response.json().then(data => {
-          app.set('isLogin', true);
-          isLogin = true;
-          userId = data.id;
-          userNum = data.number;
-          userMail = data.mail;
-          userPassword = data.password;
-          //res.cookie('data-user', { mail: data })
-          res.redirect("/home");
+          console.log("user login: " + data.id);
+          loggedUsers.push(data.id);
+          res.redirect("/home/" + data.id + "/" + data.number);
         })
       } else res.redirect("/");
+    })
+});
+
+//SIGNUP REQUEST
+router.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+router.post("/signup", function (req, res) {
+  //Register
+  fetch(API + "users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        number: req.body.number,
+        mail: req.body.mail,
+        password: req.body.password
+      }),
+    })
+    .then((response) => {
+      response.json().then(data => {
+        if (data) {
+          //Login
+          fetch(API + "login", {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                mail: req.body.mail,
+                password: req.body.password
+              }),
+            })
+            .then((response) => {
+              if (response.ok) {
+                response.json().then(data => {
+                  console.log("user created: " + data.id);
+                  loggedUsers.push(data.id);
+                  res.redirect("/home/" + data.id + "/" + data.number);
+                })
+              } else res.redirect("/");
+            })
+        } else res.redirect("/");
+      })
     })
 });
 
--- public/js/variables.js
+++ public/js/variables.js
@@ -1,9 +1,6 @@
-var currentTheme = localStorage.getItem('data-theme');
+let currentTheme = localStorage.getItem('data-theme');
 
 const tweetsAPI = "https://numerous-server.herokuapp.com/api/v1/tweets";
 const usersAPI = "https://numerous-server.herokuapp.com/api/v1/users";
 
-var userID = "2cbebc56-bc0b-4464-8e99-16f5525dfcad";
-var userNumber = "12345670";
-var userMail = null;
-var userPassword = null;
+
